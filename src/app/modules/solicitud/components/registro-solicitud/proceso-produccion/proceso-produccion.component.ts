20a41f50f524bdfb13fca3acc6f2346e9d9277bb
luisao <hanna9653@hotmail.com>
2021-01-27 17:18:38 -0500

    Se realizan validaciones

--- src/app/modules/solicitud/components/registro-solicitud/proceso-produccion/proceso-produccion.component.ts
+++ src/app/modules/solicitud/components/registro-solicitud/proceso-produccion/proceso-produccion.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import {AdjuntarArchivoComponent } from '../../adjuntar-archivo/adjuntar-archivo.component';
 import { InformativoComponent } from '../../informativo/informativo.component';
@@ -11,6 +12,13 @@ import { InformativoComponent } from '../../informativo/informativo.component';
 export class ProcesoProduccionComponent implements OnInit  {
 
   public maquila: boolean;
+
+  public form = new FormGroup ({
+    razonSocial: new FormControl('', [Validators.minLength(3), Validators.maxLength(300)]),
+    nombreContacto: new FormControl('', [Validators.minLength(3), Validators.maxLength(6000)]),
+    email: new FormControl('', [Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+    telefono: new FormControl('', [Validators.required, Validators.min(7), Validators.maxLength(11)]),
+  })
 
   public constructor(
     private dialog: MatDialog
