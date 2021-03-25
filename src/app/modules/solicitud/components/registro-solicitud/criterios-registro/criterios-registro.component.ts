94ebced121547bb81c08f856db130e4cd9fe8198
Erwing Candelario <ingeniero_79@hotmail.com>
2021-03-25 18:20:13 -0500

    Hu-11 adapatando html

--- src/app/modules/solicitud/components/registro-solicitud/criterios-registro/criterios-registro.component.ts
+++ src/app/modules/solicitud/components/registro-solicitud/criterios-registro/criterios-registro.component.ts
@@ -1,12 +1,89 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-criterios-registro',
   templateUrl: './criterios-registro.component.html',
-  styleUrls: ['./criterios-registro.component.scss']
+  styleUrls: ['./criterios-registro.component.scss'],
 })
-export class CriteriosRegistroComponent {
+export class CriteriosRegistroComponent implements OnInit {
 
-  @Input() public tipoFormulario: string;
+  inTipoFormulario: string = '';
+  mostrarInsumo: boolean = false;
 
+  @Input()
+  public set tipoFormulario(tipo: string) {
+    this.inTipoFormulario = tipo;
+    this.updateFormulario(tipo);
+  }
+
+  formularioCriterio: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.formularioCriterio = this.fb.group({
+      bienesTotal: [false],
+      bienesElaborados: [false],
+      porcentajeMinimo: [false],
+      procesoProductivo: [false],
+    });
+
+    this.formularioCriterio.controls.bienesElaborados.valueChanges.subscribe(
+      (x) => {
+        if (x) {
+          this.formularioCriterio.controls.porcentajeMinimo.disable();
+          this.formularioCriterio.controls.procesoProductivo.disable();
+        } else {
+          this.formularioCriterio.controls.porcentajeMinimo.enable();
+          this.formularioCriterio.controls.procesoProductivo.enable();
+        }
+      }
+    );
+
+    this.formularioCriterio.controls.porcentajeMinimo.valueChanges.subscribe(
+      (x) => {
+        if (x) {
+          this.formularioCriterio.controls.porcentajeMinimo.disable();
+          this.formularioCriterio.controls.procesoProductivo.disable();
+        } else {
+          this.formularioCriterio.controls.porcentajeMinimo.enable();
+          this.formularioCriterio.controls.procesoProductivo.enable();
+        }
+      }
+    );
+
+    this.formularioCriterio.controls.procesoProductivo.valueChanges.subscribe(
+      (x) => {
+        if (x) {
+          this.formularioCriterio.controls.porcentajeMinimo.disable();
+          this.formularioCriterio.controls.procesoProductivo.disable();
+        } else {
+          this.formularioCriterio.controls.porcentajeMinimo.enable();
+          this.formularioCriterio.controls.procesoProductivo.enable();
+        }
+      }
+    );
+  }
+
+  updateFormulario(tipo: string) {
+
+    this.mostrarInsumo = false;
+    
+    switch (tipo) {
+      case 'produccionNacional':
+        break;
+      case 'fomentoIndustriaAutomotriz':
+        this.formularioCriterio.controls.bienesTotal.disable();
+        this.formularioCriterio.updateValueAndValidity();
+        break;
+      case 'regimenTransformacionEnsamblePlanillas':
+        this.mostrarInsumo = true
+        break;
+
+      default:
+        break;
+    }
+  }
 }
